9f1c729c8830af1e76cdcb76887d2a836bab7305
ultramanbabe <ultraman_babeclub@hotmail.com>
2020-01-17 16:23:34 +0700

    Done

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 char before(char x){
 	//Write your function definition here
+	if(x == 'A'){
+		return 'Z';
+	}
+	if(x > 65 && x <= 90){
+		return x - 1;
+	}else{
+		return '0';
+	}
 }
 
 int main(){
